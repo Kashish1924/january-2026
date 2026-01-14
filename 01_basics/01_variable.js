752a39012503adca4a5d60d1a05b1bc2f5443baa
kashish kumari <kashishkumari335@gmail.com>
2026-01-14 16:01:47 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accId=27639
+let accEmail="kash@gmail.com"
+var accPassword="76737"
+accCity="jaipur"
+//accCity="jaipur" (ase bhi defined ho skt hai )
+
+//accId=536e5 not allowed (dubara const mai assign nhi kr skte)
+//let or varible mai let use krenge ,abhi kai liye bs( block scope and functional block issue ki vajh sai let use krte hai )
+
+//new style for print 
+console.table([accEmail,accId,accPassword,accCity])
+
+//let acc;   (it will give undeifned if i dont assign any values)
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+//javscript ka engine =browser kai ander hai 
+
+"use strict"; //treat all js code as new version
+//alert(3+3) not run becuse we r using nodejs not browser
+console.log(3+
+    3) //code readability should be high
+
+let name="kashish" //string datatype
+let age=20      //number
+let isLoggedIn=false  //boolean
+
+
+/*All datatype
+number=>2 to power 53
+bigint
+string =>""
+boolean=>true/false
+null=>standalone value
+undefined=>
+symbol=>unique
+
+object
+
+
+
+
+*/
+
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
