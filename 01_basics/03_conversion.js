c005bbaa2643436d6a46c83014427bfba9aed9a5
kashish kumari <kashishkumari335@gmail.com>
2026-01-14 16:44:49 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,30 @@
+let score = "33" //33
+
+
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//let score="33abc"  //NaN but type number
+//let score=NULL //0
+//let score=undefined  //undefined but type number 
+//true=>1
+
+let isLoggedIn="hitesh"
+let  booleanIsLogged=Boolean(isLoggedIn)
+console.log(booleanIsLogged)
+
+//1=>true; 0=>false
+//""=>false
+//"kashish"=>true
+
+let someNumber=33
+let stringNumber=string(someNumber)
+console.log(stringNumber)   //33
+console.log(typeof stringNumber); //string
