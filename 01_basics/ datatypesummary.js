3acf7abf0e851d0dccbb6e1fd785a562481c6743
kashish kumari <kashishkumari335@gmail.com>
2026-01-15 16:17:51 +0000

    summary

---
+++ 01_basics/ datatypesummary.js
@@ -0,0 +1,53 @@
+//primitive datatype
+
+//7 datatype:- string,number,boolean,BigInt,null,undefined,symbol
+
+const score=100
+const scoreValue=100.3
+//console.log(typeof score)
+
+const isLoggedIn =false //bool
+const outsideTemp=null //null
+let useremail; //undefined
+const id=symbol('123')
+const anotherId=symbol('123')
+
+//console.log(id===anotherId); false
+
+const bigNumber=2234567234567234567n
+//console.log(typeof bigNumber) //undefined
+//n lga diya bigint  bn gya
+
+//refernces(Non primitive datatpe)
+
+//array,objects,function
+
+const heros=["pg,","hbsxhj"];
+let myObj={
+    name:"kashish",
+    age:20,
+}
+const myFunction=function(){
+    console.log("hello");
+}
+console.log(typeof myFunction) //function
+
+/*Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object */
+
+
+
+
+
