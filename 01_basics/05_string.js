8e3b57971bbed23ba5c36d2124e0f06c758819e0
kashish kumari <kashishkumari335@gmail.com>
2026-01-27 16:40:11 +0000

    commit

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+const name="hitesh"
+const repoCount= 40
+console.log(name+ repoCount);
+console.log('hello my name is ${name} and my repo count is${repoCount}');
+console.log(name[0]);
+console.log(name.__proto__);
+console.log(name.toUpperCase());
+
+console.log(name.charAt(2));
+console.log(name.indexOf('k'));
+
+console.log(name.length);
