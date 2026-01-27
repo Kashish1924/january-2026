7e28f807e458ace3032c89e5650b841d0a25e788
kashish kumari <kashishkumari335@gmail.com>
2026-01-27 17:35:06 +0000

    mm

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,4 +1,6 @@
+
 const name="hitesh"
+const gameName=new String("hitesh-hc")
 const repoCount= 40
 console.log(name+ repoCount);
 console.log('hello my name is ${name} and my repo count is${repoCount}');
@@ -6,7 +8,18 @@ console.log(name[0]);
 console.log(name.__proto__);
 console.log(name.toUpperCase());
 
+
 console.log(name.charAt(2));
 console.log(name.indexOf('k'));
 
 console.log(name.length);
+const newString=name.substring(0,2)
+console.log(newString);
+const anotherString=name.slice(-2,2)
+console.log(anotherString);
+const newStringOne= "   kashu    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+const url='https://hitesh.com/hitesh%20choudhary'
+console.log((url.replace('%20','-')))
+console.log(url.includes('sundar'));
